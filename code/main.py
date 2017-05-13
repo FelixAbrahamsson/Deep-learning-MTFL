98ffca3eccd1c5af3a19af9c90ff87dbb72144cb
Felix <felixab@kth.se>
2017-05-13 23:20:19 +0200

    merging nothing really...

--- code/main.py
+++ code/main.py
@@ -12,6 +12,6 @@ data = DataReader(dataFolder, train_txt, "testing.txt")
 
 network = CNNSingle(data, 50, -1) #batch size, landmark
 # network.debugNetwork()
-sess = network.trainNetwork(2, 1.0) # epochs, keep_prob
+sess = network.trainNetwork(75, 1.0) # epochs, keep_prob
 network.testNetwork(sess)
 network.outputImages(sess)
