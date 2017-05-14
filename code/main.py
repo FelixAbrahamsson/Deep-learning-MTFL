575e0e3eafbb07b6a3f5ed3fcd1a15ea4c0aa147
Felix <felixab@kth.se>
2017-05-14 08:50:10 +0200

    Merged the two main files

--- code/main.py
+++ code/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import tensorflow as tf
 from InputPipeline import DataReader
 from singleTask import CNNSingle
+from singleTask_att import CNNSingleAtt
+
 
 train_txt = "training.txt"
 # train_txt = "aug_training.txt"
@@ -11,6 +13,8 @@ dataFolder = os.path.abspath(os.path.join("./", os.pardir)+"/MTFL")
 data = DataReader(dataFolder, [train_txt, "validation.txt", "testing.txt"])
 
 network = CNNSingle(data, 50, -1) #batch size, landmark
+# network = CNNSingleAtt(data, 50, 2) #batch size, attribute
+
 # network.debugNetwork()
 sess = network.trainNetwork(3, 1.0) # epochs, keep_prob
 network.testNetwork(sess)
